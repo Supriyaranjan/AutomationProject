35b27dacc22470b8d3345a76004b46fe8433be23
abhishek <supriyaranjan.mit@gmail.com>
2017-05-08 12:28:08 -0500

    Automation Project started

---
+++ AutomationProject/src/testcases/Testcase1.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	@Test
+	public void testcase1(){
+		
+
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElementById("email").sendKeys("data1");
+		driver.findElementById("password").sendKeys("data2");
+		driver.quit();
+		
+			
+		
+		
+	}
+
+}
